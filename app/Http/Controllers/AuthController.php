3e550c83119c8823ac7242cf6d0d048cde13b757
samyush <samyushmaharjan@gmail.com>
2020-10-23 15:31:24 +0545

    first commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,91 +1,106 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Http\Requests\RegisterAuthRequest; use App\User;
+
+use App\Http\Requests\RegisterAuthRequest;
+use App\User;
 use Illuminate\Http\Request;
-use JWTAuth;
+use Tymon\JWTAuth\Facades\JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
 
 
- class AuthController extends Controller { 
+class AuthController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
 
     public $loginAfterSignUp = true;
 
-     public function register(Request $request) { 
-         $user = new User();
-         $user->name = $request->name;
-         $user->email = $request->email; 
-         $user->password = bcrypt($request->password); 
-         $user->save();
+    public function register(Request $request)
+    {
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
 
 
-         if ($this->loginAfterSignUp) { 
-             return $this->login($request);
-            }
- 
-            return response()->json([ 
-                'status' => 'ok', 'data' => $user
-            ], 200); 
+        if ($this->loginAfterSignUp) {
+            return $this->login($request);
         }
 
+        return response()->json([
+            'status' => 'ok', 'data' => $user
+        ], 200);
+    }
 
 
-        public function login(Request $request) { 
 
-            $input = $request->only('email', 'password'); 
-            $jwt_token = null;
+    public function login(Request $request)
+    {
 
-            if (!$jwt_token = JWTAuth::attempt($input)) {
+        $input = $request->only('email', 'password');
+        $jwt_token = null;
+
+        if (!$jwt_token = JWTAuth::attempt($input)) {
             return response()->json([
-            'status' => 'invalid_credentials',
-            'message' => 'Correo o contraseña no válidos.',
-            ], 401); 
-            }
-            return response()->json([ 
-                'status' => 'ok', 
-                'token' => $jwt_token,
-            ]); 
+                'status' => 'invalid_credentials',
+                'message' => 'Correo o contraseña no válidos.',
+            ], 401);
         }
+        return response()->json([
+            'status' => 'ok',
+            'token' => $jwt_token,
+        ]);
+    }
 
-        
 
 
-        public function logout(Request $request) { 
-            $this->validate($request, [
-                'token' => 'required' ]);
-            
-                try { 
-                JWTAuth::invalidate($request->token); 
-                return response()->json([
+
+    public function logout(Request $request)
+    {
+        $this->validate($request, [
+            'token' => 'required'
+        ]);
+
+        try {
+            JWTAuth::invalidate($request->token);
+            return response()->json([
                 'status' => 'ok',
-                'message' => 'Cierre de sesión exitoso.' ]);
-                    } catch (JWTException $exception) { 
-                        return response()->json([
-                        'status' => 'unknown_error',
-                        'message' => 'Al usuario no se le pudo cerrar la sesión.' ], 500);
-                            } 
+                'message' => 'Cierre de sesión exitoso.'
+            ]);
+        } catch (JWTException $exception) {
+            return response()->json([
+                'status' => 'unknown_error',
+                'message' => 'Al usuario no se le pudo cerrar la sesión.'
+            ], 500);
         }
+    }
 
 
 
 
-        public function getAuthUser(Request $request) { 
-            $this->validate($request, [
-                'token' => 'required' ]);
-
-                $user = JWTAuth::authenticate($request->token); 
-                return response()->json(['user' => $user]);
-
-            }
+    public function getAuthUser(Request $request)
+    {
+        $user = auth()->user();
+        return response()->json(['user' => $user]);
+    }
 
 
-            protected function jsonResponse($data, $code = 200) {
+    protected function jsonResponse($data, $code = 200)
+    {
 
-                return response()->json($data, $code,
-                    ['Content-Type' => 'application/json;charset=UTF-8',
-                    'Charset' => 'utf-8'], 
-                    JSON_UNESCAPED_UNICODE); 
-                }
-
-
+        return response()->json(
+            $data,
+            $code,
+            [
+                'Content-Type' => 'application/json;charset=UTF-8',
+                'Charset' => 'utf-8'
+            ],
+            JSON_UNESCAPED_UNICODE
+        );
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 // estas rutas se pueden acceder sin proveer de un token válido.
-Route::post('/login', 'AuthController@login'); 
+Route::post('/login', 'AuthController@login');
 Route::post('/register', 'AuthController@register');
 // estas rutas requiren de un token válido para poder accederse. 
-Route::group(['middleware' => 'auth.jwt'], function () { 
+Route::group(['middleware' => 'api'], function () {
     Route::post('/logout', 'AuthController@logout');
+    Route::get('/currentUsr', 'AuthController@getAuthUser');
 });
