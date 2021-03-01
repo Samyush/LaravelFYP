d816f34d3883d3bbbff09d6513fa0a7db311e962
Samyush <samyushmaharjan@gmail.com>
2021-03-01 22:57:29 +0545

    2nd iteration

---
+++ app/Http/Controllers/YearController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Year;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class YearController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        //$years = new Year();
+
+        $years = DB::table('years')->get();
+        return $years;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return "is alo woring";
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $year = new Year();
+        $year->year = $request->year;
+        if( $year->save())
+            return "success";
+        else
+            return "failed";
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Year  $year
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Year $year)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Year  $year
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Year $year)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Year  $year
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Year $year)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Year  $year
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Year $year)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 // estas rutas se pueden acceder sin proveer de un token válido.
 Route::post('/login', 'AuthController@login');
 Route::post('/register', 'AuthController@register');
+Route::resource('year', 'YearController');
 // estas rutas requiren de un token válido para poder accederse.
 Route::group(['middleware' => 'api'], function () {
     Route::post('/logout', 'AuthController@logout');
