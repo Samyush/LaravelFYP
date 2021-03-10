d0afea74805203409f26f2c020077bb45edeec2b
Samyush <samyushmaharjan@gmail.com>
2021-03-10 15:55:20 +0545

    csv upload successful

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/try', 'PagesController@index'); // localhost:8000/
+Route::post('/uploadFile', 'PagesController@uploadFile');
